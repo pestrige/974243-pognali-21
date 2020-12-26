13fdf2028fe54f2c5a292d954f529f2442c71bb6
pestrige <pestrige@gmail.com>
2020-12-26 10:47:13 +0300

    Закончил верстку и стилизацию главной

--- source/js/mobile-menu.js
+++ source/js/mobile-menu.js
@@ -4,22 +4,22 @@
   //open-close mobile menu
 
   const body = document.querySelector('.body');
-  //const mainHeader = body.querySelector('.main-header__wrapper');
-  const mainHeader = body.querySelector('.main-header__inner');
+  const header = body.querySelector('.main-header');
+  const mainHeader = header.querySelector('.main-header__wrapper');
   const menuBtn = mainHeader.querySelector('.humburger');
   const mainNav = mainHeader.querySelector('.main-nav');
   const userLinks = body.querySelector('.main-header__user-links');
 
   menuBtn.classList.remove('humburger--open');
-  mainHeader.classList.remove('main-header__inner--white');
+  mainHeader.classList.remove('main-header__wrapper--white');
   mainNav.classList.add('main-nav--close');
 
   const openMenu = () => {
     menuBtn.addEventListener('click', () => {
-      const isFixed = mainHeader.classList.contains('main-header__inner--fixed');
+      const isFixed = mainHeader.classList.contains('main-header__wrapper--fixed');
 
       menuBtn.classList.toggle('humburger--open');
-      mainHeader.classList.toggle('main-header__inner--white');
+      mainHeader.classList.toggle('main-header__wrapper--white');
       mainNav.classList.toggle('main-nav--close');
       if (isFixed) body.classList.toggle('no-scroll');
     });
@@ -27,35 +27,58 @@
 
   //fixed menu on scroll
 
-  const fixedClass = 'main-header__inner--fixed';
+  const fixedClass = 'main-header__wrapper--fixed';
+  const fixedHeaderClass = 'main-header--fixed';
   const animationClassIn = 'animation-in';
   const animationClassOut = 'animation-out';
 
   const scrollMenu = () => {
     window.addEventListener('scroll', () => {
-      const isFixed = mainHeader.classList.contains('main-header__inner--fixed');
+      const isFixed = mainHeader.classList.contains(fixedClass) || header.classList.contains(fixedHeaderClass);
       const isClose = mainNav.classList.contains('main-nav--close');
-      const pageWidth = document.documentElement.scrollWidth;
-      const isTablet = pageWidth >= 768;
+      const pageWidth = body.scrollWidth;
+      const isTablet = (pageWidth >= 768 && pageWidth < 1440);
+      const isMobile = pageWidth < 768;
+      const isDesktop = pageWidth >= 1440;
       const y = window.scrollY;
 
       if (isClose) {
         const addFixed = () => {
-          mainHeader.classList.add(fixedClass);
-          mainHeader.classList.remove(animationClassOut);
-          mainHeader.classList.add(animationClassIn);
-          //userLinks.classList.add(animationClassIn);
+          if (isDesktop) {
+            header.classList.add(fixedHeaderClass);
+            mainHeader.classList.add(fixedClass);
+            header.classList.remove(animationClassOut);
+            header.classList.add(animationClassIn);
+          } else {
+            mainHeader.classList.add(fixedClass);
+            mainHeader.classList.remove(animationClassOut);
+            mainHeader.classList.add(animationClassIn);
+            if (!isMobile) userLinks.classList.add(animationClassIn);
+          }
         };
         const delFixed = () => {
-          mainHeader.classList.remove(animationClassIn);
-          if (isTablet) userLinks.classList.remove(animationClassIn);
-          mainHeader.classList.add(animationClassOut);
-          if (isTablet) userLinks.classList.add(animationClassOut);
-          setTimeout(() => {
-            mainHeader.classList.remove(fixedClass);
-            mainHeader.classList.remove(animationClassOut);
-            //userLinks.classList.remove(animationClassOut);
-          }, 500);
+          if (isDesktop) {
+            header.classList.remove(animationClassIn);
+            mainHeader.classList.remove(animationClassIn);
+            userLinks.classList.remove(animationClassIn);
+            header.classList.add(animationClassOut);
+            setTimeout(() => {
+              header.classList.remove(fixedHeaderClass);
+              mainHeader.classList.remove(fixedClass);
+              header.classList.remove(animationClassOut);
+              userLinks.classList.remove(animationClassOut);
+            }, 500);
+          } else {
+            mainHeader.classList.remove(animationClassIn);
+            if (isTablet) userLinks.classList.remove(animationClassIn);
+            mainHeader.classList.add(animationClassOut);
+            if (isTablet) userLinks.classList.add(animationClassOut);
+            setTimeout(() => {
+              mainHeader.classList.remove(fixedClass);
+              mainHeader.classList.remove(animationClassOut);
+              userLinks.classList.remove(animationClassOut);
+            }, 500);
+          }
         };
 
         if (y > 70) addFixed();
@@ -63,6 +86,31 @@
       };
     });
   };
+
+  // resize actions
+
+  window.addEventListener('resize', () => {
+    const bodyWidth = body.clientWidth;
+    const isClose = mainNav.classList.contains('main-nav--close');
+    const isFixed = mainHeader.classList.contains(fixedClass);
+    const isDesktopFixed = header.classList.contains(fixedHeaderClass);
+
+    if (bodyWidth >= 1440 && !isClose) {
+      menuBtn.classList.remove('humburger--open');
+      mainHeader.classList.remove('main-header__wrapper--white');
+      mainNav.classList.add('main-nav--close');
+      body.classList.remove('no-scroll');
+    };
+
+    if (bodyWidth < 1440 && isDesktopFixed) {
+      header.classList.remove(fixedHeaderClass);
+      header.classList.remove(animationClassIn);
+    };
+
+    if (bodyWidth >= 1440 && isFixed) {
+      header.classList.add(fixedHeaderClass);
+    };
+  });
 
   scrollMenu();
   openMenu();
